2ceb0cdc15e0acdf4e35f476bf8a856ff791cf45
Ferreira Anthony <matrixfrpro71230@hotmail.fr>
2023-07-26 19:34:49 +0200

    Init Project

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Sneaker Company</h1>
+        <nav>
+          <ul>
+            <li>Collections</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <h2>Fall Limited Edition Sneakers</h2>
+        <p>
+          These low-profile sneakers are your perfect casual wear companion. Featuring a 
+          durable rubber outer sole, they’ll withstand everything the weather can offer.
+        </p>
+        $125.00
+        50%
+        $250.00
+
+        0
+        Add to cart
+      </main>
+      <footer>
+        <div className="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+          Coded by <a href="#">Your Name Here</a>.
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default App
